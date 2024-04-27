7805083723719101c8efd567585aada98f72fc1a
Pedro H. Araujo <142441050+pedroarawj@users.noreply.github.com>
2024-04-27 19:13:36 -0300

    Adição da biblioteca display e modificação da lógica.ino

--- main.ino
+++ main.ino
@@ -1,60 +1,80 @@
+#include <U8glib.h>
 #include "EmonLib.h"
 
 #define Pino_Corrente A1
-#define PinVerde 9  // Pino do LED verde
-#define PinVermelho 8
 
+U8GLIB_ST7920_128X64_1X u8g(11, 13, 12, U8G_PIN_NONE);
 EnergyMonitor emon;
 
-float valor_tensao = 5.0; // Tensão para o cálculo da corrente
-double potencia; 
+char corrente_tela[6];
+char potencia_tela[6];
+float valor_tensao = 5.0;
+double potencia;
 double energia;
-float custo_energia;
-float limite_custo = 0.01;
+double custo_energia;
+double potencia_tot = 0;
+double potencia_media = 0;
+int contador = 0;
+double energia_total = 0;
+double custo_total = 0;
 unsigned long tempo_anterior = 0;
 
 void setup() {
-  Serial.begin(9600);
+  u8g.setFont(u8g_font_6x10);
 
-  emon.current(Pino_Corrente, 60.6); // Calibração do sensor para a análise da corrente
-  pinMode(PinVerde, OUTPUT);
-  pinMode(PinVermelho, OUTPUT);
+  u8g.begin();
+
+  emon.current(Pino_Corrente, 60.6);
 }
 
 void loop() {
+  contador++; // Conta quantas vezes ocorreu um loop para o cálculo da potência média
   unsigned long tempo_atual = millis();
-  double corrente = emon.calcIrms(1480); // A corrente é calculada com auxílio da biblioteca
-  potencia = corrente * valor_tensao; // Definição de potência
-  energia = potencia * (tempo_atual - tempo_anterior) / 3600 / 1000; // Consumo de energia em kwh. Conversão da potencia e do tempo
-  custo_energia = energia * 0.6; // Cálculo do custo de energia com base na tarifa de João Pessoa
+  double corrente = emon.calcIrms(1480); //Calcula a corrente que passa pelo fio
+  potencia = corrente * valor_tensao; // Calcula a potência do dispositivo
+  potencia_tot += potencia; // Recebe o total dos valores das potências apresentadas
 
-  Serial.print("Corrente: ");
-  Serial.print(corrente);
-  Serial.println(" A");
+  potencia_media = potencia_tot / contador; // A média entre as potências apresentadas durante o loop 
+  energia_total = potencia_media * (tempo_atual / 60); // Calcula o consumo de energia com a média das potências vezes o tempo de uso do dispositivo (seg para h)
+  custo_energia = energia_total * 0.6; //Calcula o custo do consumo de acordo com a tarifa da cidade de João Pessoa
+  char custo[6];
+  dtostrf(custo_energia, 5, 2, custo); // Transforma de ponto flutuante para string (para exibir no display)
+  
+  numpotpstr(potencia);
+  dtostrf(corrente, 4, 1, corrente_tela); // 4 define o tamanho max da string e 1 é a quantidade de "numeros" apos o ponto
 
-  Serial.print("Potência: ");
-  Serial.print(potencia, 1);
-  Serial.println(" W");
-  delay(1000);
+  char energia_tela[6];
+  dtostrf(energia_total, 5, 2, energia_tela);
 
-  Serial.print("Consumo: ");
-  Serial.print(energia, 3);
-  Serial.println(" kWh");
-  Serial.print("Custo: ");
-  Serial.print(custo_energia, 2);
-  Serial.println(" reais");
-  delay(1000);
+  u8g.firstPage(); // Mostra os valores da corrente e potência no display
+  do {
+    u8g.setFont(u8g_font_unifont);
+    u8g.drawStr(0, 20, "Corrente:");
+    u8g.drawStr(70, 20, corrente_tela);
+    u8g.drawStr(105, 20, "A");
+    u8g.drawStr(0, 60, "Potencia:");
+    u8g.drawStr(75, 60, potencia_tela);
+    u8g.drawStr(108, 60, "W");
+  } while (u8g.nextPage());
 
-// Estrutura condicional que depende do custo e limite fornecido
-  if (custo_energia > limite_custo){
-    digitalWrite(PinVerde, LOW);
-    digitalWrite(PinVermelho, HIGH); // Se o custo exceder o limite, o led vermelho acenderá
-    delay(500);
-  } else {
-    digitalWrite(PinVermelho, LOW);
-    digitalWrite(PinVerde, LOW);
-    delay(500);
-    digitalWrite(PinVerde, HIGH); //Se o custo não exceder o limite, o led verde ficará piscando
-  } 
+  delay(60000); // Tempo de 1 minuto o consumo seja calculado 
 
+  u8g.firstPage(); // Mostra os valores do consumo e custo no display
+  do {
+    u8g.setFont(u8g_font_unifont);
+    u8g.drawStr(0, 20, "Consumo: ");
+    u8g.drawStr(55, 20, energia_tela);
+    u8g.drawStr(100, 20, "kwh");
+    u8g.drawStr(0, 60, "Custo: ");
+    u8g.drawStr(55, 60, "R$");
+    u8g.drawStr(64, 60, custo);
+  } while (u8g.nextPage());
+
+  delay(3000); // Tempo para exibir os valores de corrente e potência novamente
 }
+
+void numpotpstr(double pot){
+  dtostrf(pot, 4, 1, potencia_tela);// Transforma de ponto flutuante para string (para exibir no display)
+}
+
+
